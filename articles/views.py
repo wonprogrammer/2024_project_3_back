4640bc6187342fe82432b7c8eee2ced4a86cc9f7
leehyewon <tnddl2345@gmail.com>
2024-07-15 18:20:09 +0900

    ADD articles app & url, views setting

---
+++ articles/views.py
@@ -0,0 +1,45 @@
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+
+class ArticleView(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ArticleDetailView(APIView):
+    def get(self, request, article_id):
+        pass
+
+    def put(self, request, article_id):
+        pass 
+
+    def delete(self, request, article_id):
+        pass 
+
+
+class CommentView(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class CommentDetailView(APIView):
+    def put(self, request):
+        pass 
+
+    def delete(self, request):
+        pass 
+
+
+class LikeView(APIView):
+    def post(self, request):
+        pass
--- drf_3/settings.py
+++ drf_3/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'users',
+    'articles',
 ]
 
 REST_FRAMEWORK = {
