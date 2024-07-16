2439fdb175df0a6dbe474d955e3309f16b1804ef
leehyewon <tnddl2345@gmail.com>
2024-07-16 17:20:42 +0900

    ADD articles model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # 1:다 의 관계 (하나의 유저가 여러개의 게시글 작성 가능)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
